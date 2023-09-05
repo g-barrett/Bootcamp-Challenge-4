4006b1953627c4d0e8abfd7884e5e14c9e1947ef
g-barrett <barrettg516@gmail.com>
2023-09-05 18:43:25 -0500

    structured and styled page with html and css

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+var questions = [
+    {
+        question: 'Which of these is NOT primitive?',
+        answers: [
+            { text: 'Boolean', correct: false},
+            { text: 'Object', correct: true},
+            { text: 'Number', correct: false},
+            { text: 'Undefined', correct: false}
+        ]
+    }
+]
